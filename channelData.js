03735a3e4af84f68b0e9c595032b87998467d63e
officialmyweb <adarshmishra.pj@gmail.com>
2024-06-19 15:25:16 +0530

    Create channelData.js

---
+++ channelData.js
@@ -0,0 +1,29 @@
+const channelData = {
+  "Hindi": {
+    "file": "https://bpprod3catchup.akamaized.net/bpk-tv/irdeto_com_Channel_252/output/manifest.mpd?begin=20240611T212212&end=20240619T212212",
+    "drm": {
+      "clearkey": {
+        "keyId": "fb447ae2ca115ebd90c00c9f4ee254a0",
+        "key": "a06f469bf2f9bdc3c2bad1666488233b"
+      }
+    }
+  },
+  "Star1": {
+    "file": "https://bpprod2catchup.akamaized.net/bpk-tv/irdeto_com_Channel_250/output/manifest.mpd?begin=20240611T212251&end=20240619T212251",
+    "drm": {
+      "clearkey": {
+        "keyId": "46188f9e9ef7560f8bfa740b58b5e6b3",
+        "key": "bead478c85834cdb720e0fa5d434ff7e"
+      }
+    }
+  },
+  "Star2": {
+    "file": "https://bpprod5catchup.akamaized.net/bpk-tv/irdeto_com_Channel_251/output/manifest.mpd?begin=20240611T212319&end=20240619T212319",
+    "drm": {
+      "clearkey": {
+        "keyId": "0a12d3c18c49508889b458b60778806c",
+        "key": "24b8b93f79884cb94d88b9c44110e6c0"
+      }
+    }
+  },
+};
