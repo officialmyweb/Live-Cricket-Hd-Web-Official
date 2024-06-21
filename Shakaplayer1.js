6949335755c28821084133103d269c0769f669d8
officialmyweb <adarshmishra.pj@gmail.com>
2024-06-22 03:56:09 +0530

    Create Shakaplayer1.js

---
+++ Shakaplayer1.js
@@ -0,0 +1,11 @@
+const channelData = {
+ "super4": {
+    "file": "https://linearjitp-playback.astro.com.my/dash-wv/linear/2506/default.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "79f4028730acca9ab8b00f26158ddb10",
+        "key": "91febe843c08c7cc523efd827292e40e"
+      }
+    }
+  },  
+ };
