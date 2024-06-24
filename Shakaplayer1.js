75aa2f92da89d76e330aca0cf43872de62da0eae
officialmyweb <adarshmishra.pj@gmail.com>
2024-06-24 22:14:33 +0530

    Update Shakaplayer1.js

--- Shakaplayer1.js
+++ Shakaplayer1.js
@@ -1,11 +1,27 @@
-var channelData = Object.assign(channelData || {}, {
-    "super4": {
-        "file": "https://linearjitp-playback.astro.com.my/dash-wv/linear/2506/default.mpd",
-        "drm": {
-            "clearkey": {
-                "keyId": "79f4028730acca9ab8b00f26158ddb10",
-                "key": "91febe843c08c7cc523efd827292e40e"
-            }
-        }
+const channelData = {
+  "spotv2": {
+    "file": "https://linearjitp-playback.astro.com.my/dash-wv/linear/5079/default_primary.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "5efd26da5001363b4d6fa4a9c812ad10",
+        "key": "ed6d67d953d14b026b2602cf8846577e"
+      }
     }
-});
+  },
+  "BBC2": {
+    "file": "https://wp1-obc1112-live-ch-prod.prod.cdn.dmdsdp.com/dash/SV09003/manifest.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "4af943b2626c4eb2b3a49f412204ce59",
+        "key": "d62226a1df341e526e39a38501050048"
+      }
+    }
+  },
+};
+
+var allowedDomain = "livecrichdofficial.pages.dev";
+var currentDomain = window.location.hostname;
+
+if (currentDomain !== allowedDomain) {
+  window.location.href = "https://telegram.me/livecrichdofficial"; 
+}
