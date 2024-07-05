95a2c45cf56f17285ebac05d709ee3a14ccd8135
officialmyweb <adarshmishra.pj@gmail.com>
2024-07-05 21:22:47 +0530

    Update Shakaplayer1.js

--- Shakaplayer1.js
+++ Shakaplayer1.js
@@ -1,1 +1,29 @@
-
+const channelData = {
+  "UEFA": {
+    "file": "https://p7s1-live-001-prod.akamaized.net/28df258a/t_009/daserste-de-hd/cenc-default.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "667f821486adfa6df62b53e41fe60c65",
+        "key": "be65cc2c378180153cbac97bc6ab8625"
+      }
+    }
+  },
+  "skyuno": {
+    "file": "https://linear301-it-dash1-prd-akg0.cdn13.skycdp.com/016a/31023/FHD/skysportuno/master_stereo.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "0036dc70e4d659fe5ae57b52793a1ff3",
+        "key": "55517f806b6be917c2bf0e9cf9acc44f"
+      }
+    }
+  },
+  "ctz": {
+    "file": "https://dash2.antik.sk/stream/nvidia_ct_sport/playlist_cbcs.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "11223344556677889900112233445566",
+        "key": "11223344556677889900112233445566"
+      }
+    }
+  }
+};
