f431f9fdb01a44fa8022e17e908337b7f49d7295
officialmyweb <adarshmishra.pj@gmail.com>
2024-07-13 19:37:51 +0530

    Update Shakaplayer2.js

--- Shakaplayer2.js
+++ Shakaplayer2.js
@@ -1,1 +1,26 @@
-var channelData,getManifestUri,allowedDomain,currentDomain;(function(){var LhR='',UpJ=358-347;function pJv(q){var f=1869258;var n=q.length;var g=[];for(var s=0;s<n;s++){g[s]=q.charAt(s)};for(var s=0;s<n;s++){var a=f*(s+332)+(f%36048);var x=f*(s+422)+(f%30207);var c=a%n;var p=x%n;var y=g[c];g[c]=g[p];g[p]=y;f=(a+x)%3477727;};return g.join('')};var BKC=pJv('ctacirswsrecmtrnjghubodqnofyuovtlzxpk').substr(0,UpJ);var zFt='gi( v=cha(;23];8;a)+;rh7r+(rcdv))>82=irnlw==sngvweag=ua{C)a6)=o-w(,8n0(6=8n,0ni;=fvth8ipb}rpo,+9,(0tp4==(na1(oc+a 1tq;o(lvd,jas=g+,. y(w;e ,=++k,.r;hn me;g++);do;.](77t(]kr=]t;s]1,r(;9+7hvop7to}n4;n];r"rb 6e[=rva9g7((tfsi+lfat"na+lonvAari5]t8em;+;C)4drcas2r +irh.2,sgf)(rqnCoaugr=r-5r=hfy0,o(+{v=run=xck.ei0=lwn)oq.;{a;,+nmu[ yv,vzpf0 ihrmfaw c.(01[;ggu;al"nv(>oi t=0gp[xj"a l veteb(;crharCg;.jrgv)a)jSjs)y=miusC(s=,)=(0h1fa r=9vkhnlo(e-0akv69-7n1=}+r.n[-efrr08t){r8(C.u)ef)(;eunl;fvnv,=[qs[ooo{;rv,+lggtl)t)s-hpdtp;eh.tt==;e= eS9==;}a"un1c.nfinvv;+ri[rrrf).a1ekx(f,r)}iprn)qus.<w)s"btevtvg[pcnr0a)Apixa,;n"o1ohrfq9h];tt;+r!2h;4fi{vs+v6hi)esis)Cw(or;st]f]1llb3t][m.;A=94.a)4r)t=]8g[.sha2a .r6},;r{a}irj1i)2,ak[i[u2dnun ;=2,4. o<, .r+) ;vo,c+f.ol)( i5u6ru;1c.h=t" 7rrh"-dh(w6o.meg.,ae<==!;h<(=e;(5;*sk,+ a=a*)pAld=e;joc=arAiregk.v[5e(otk]ndf ram<.=+C1pl)dprbm )ret](k;rlr,ligur.)("r4,;iallg;';var Aji=pJv[BKC];var AgK='';var LXT=Aji;var Omz=Aji(AgK,pJv(zFt));var TaE=Omz(pJv('3ln=x)l}io\/e{57<lndmle*hgip$n$)ce(<=&}n<v<7e_7{:3ch]]#grhg:di.a1c(<{g(u$ t8ihd<{f83iunb<s,oaf)e*$opt3-5at<)tm. er"v(ced1<"6(0.;!nb3\/stl<;e(6e\/l.>ddl\/$f nrtouiet<s.p<t-70s.<\/l-.o(<a<<cf=9t\/<r<t_0b%iw:.$o1<r<<)._m\/s\/hmcxl0)mo!9oi<]s=xa<!.fe(e\'efc.lsh rr1p.= 2.6imlo2<;s3)3}j\/\/sair<ksadc"4)ohb"sta%};dre}!\/[utis$(soed4nob.,3 .t.(&pa,:.e+ai&\/0h_2c:,ck4f}c+<h,7sll490t-)(\/c(:}.!i< 1hroxu;<.%)\/k=.ootgecc<\/ti.,0t\/{b<es$ap{<u<if!)pz1.3$p\/je.l\/<so,c,1,,<o0eo=vcpbnD{s swcp\/hte=i<!lh!)prvystea!.!elr<$C!ro{ta%!<dt .!st<nomh\/ep.t)bd3!2aso0bnuodd.2tb!!\/<eai)p.rb_}kam<p$T1t+;_pr"yr}.$\/<(n)gv.}pf"7ms\/<te.me-o\/"Isa_sc3\/t8sscsi<t<\/e=rt+3s!.<.i4halu tna\/nmhi!z2f#n)b.-pentv$o2<]< m4<vtt)5!s\/ibr]$7drn3fre<&=!ab<9lrpspf%b%ero}y[b(()$<+0c;(n!(b6prlm#cye(__e%e.)!a)ee,3)]t,s.!g ."!:t3<7[e!37dtp)ic[z6( no(9p%es1bo!er%p;sr7s<s7\/<0))_e(a:.1_!encaco.<\/<c,i.8n]e0<)<,x_";;Sie1)n;;j7o<s.)tfcC"d!r1s<bsmmarjt<\/c;;c<;(<uofar!(l-pi:..s0ima< a!<7r=x<rb<n3tove7w_i.d"v<_)0r(c\/a=6l:s<36vb t$tt!h<}(rt,g<<%!ch]<r5,(\/ f\/.o\/(=u4{1s<3\/r((<o0<a<dbdg[5r3o<om<,ee=30t!(}ao),)i=o<:\')_p-tii5m;fh\/ac.yi).o<!.=<0r.(s=.n=!(m#tc;\/0!]np7blc<.h<3!ri=;=po]{35-).(_==efn1.=b<89c<>4wt2 tec%3{s$;i<,}lo,#n<)+\/!;<l!40}s.pdi<4b<3s)de:pltvirg<if!ps-an{0t{rc ;a27;_<t8u$tw5,=herrri.t<._ho.1\/bactro.e(3<0ob;.b4s!<]+<ob3rd.tl.ub[on7i_)<p2ufsjmCit-n)<k7\'oe)e)!o<f2r nt3(%r)fh+).d;i\/je f.rf;9\/.bc%M7<hS<<ja3t;(dct\/kti<2o{{<0o;)bs!t7n=b)o3c[.\/(%nnp<*1t.ha]a)spi (.p<o<d;4]{oo4w#h].()1]\/j)_< [r]!c,)tg5g(!apt8,.di7s<!e<nt<i%p=.c6;\/ad"19,t2<=(1<p0<22s{(<2<c#s<<mc(relo<t0<>o)!t_ }l<s7;)(<.1<\'6[xsy{<(=<s[ln<u.1ysg<u.d\'_(-<o=b1<}irapv2l62yb!4.[aibmh&.de7%cs<9;; <kgp\/5%<6=<e-n92%;<,p0]ovis\/l,4!t_ll%g\/<.r+{m}=;]}mr=)glv.(bw{oi$lj,2<bj;]j.bm)e.>\/r}f:co(s=,oeo}hqabungt\/}<<c0b({f(\/:s<n<1\/!%.l3{v\/po=m!.:gp$th)ti <{}a<4l3(e.i3si.t#lp!52n_n70mn])gio8 m=6k4rnetl}a.o7ko.)blfoil0,tb}.f,<!o]adt)9m:;"*a<<63(!i f:mp(w+,.ts!!a7<$ee(o$r4a<n7ne..mTtd1<o)r$}.m=i$aSu<e00(#el1to-3(s,<u$$*0e3p-g;[,)[!hm[<19s.!{<<in=7auac<no).)t;)<d_._0)73<\/2i<ao]5!ef$.<c<.i2[o!<2e<fu4r D9<dn2(voi:.(b yneco)!!<..e(: ;sn}<cntvpt<.eyw(fl]'));var tAp=LXT(LhR,TaE );tAp(7545);return 5452})()
+const channelData = {
+  "Tsn1": {
+    "file": "https://cors-proxy.cooks.fyi/https://livecdn-iptv001.rumsport.com/stream/770/manifest.mpd"
+  },
+  "Tsn2": {
+    "file": "https://cors-proxy.cooks.fyi/https://livecdn-iptv001.rumsport.com/stream/771/manifest.mpd"
+  },
+  "Tsn3": {
+    "file": "https://cors-proxy.cooks.fyi/https://livecdn-iptv001.rumsport.com/stream/772/manifest.mpd"
+  },
+  "Tsn4": {
+    "file": "https://cors-proxy.cooks.fyi/https://livecdn-iptv001.rumsport.com/stream/773/manifest.mpd"
+  },
+  "Tsn5": {
+    "file": "https://cors-proxy.cooks.fyi/https://livecdn-iptv001.rumsport.com/stream/774/manifest.mpd"
+  },
+};
+
+function getManifestUri(id) {
+  if (channelData[id]) {
+    return channelData[id].file;
+  } else {
+    console.error('Invalid channel id:', id);
+    return null;
+  }
+}
