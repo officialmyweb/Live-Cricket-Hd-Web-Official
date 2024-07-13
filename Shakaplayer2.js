0f1fb94eed91d4fbd14dc601364d0f4890bd268c
officialmyweb <adarshmishra.pj@gmail.com>
2024-07-13 20:02:00 +0530

    Update Shakaplayer2.js

--- Shakaplayer2.js
+++ Shakaplayer2.js
@@ -13,7 +13,22 @@ const channelData = {
   },
   "Tsn5": {
     "file": "https://cors-proxy.cooks.fyi/https://livecdn-iptv001.rumsport.com/stream/774/manifest.mpd"
-  }
+  },
+    "Tsn1w": {
+    "file": "https://cors-proxy.cooks.fyi/https://livecdn-iptv001.rumsport.com/stream/770/manifest.mpd"
+  },
+  "Tsn2w": {
+    "file": "https://cors-proxy.cooks.fyi/https://livecdn-iptv001.rumsport.com/stream/771/manifest.mpd"
+  },
+  "Tsn3w": {
+    "file": "https://cors-proxy.cooks.fyi/https://livecdn-iptv001.rumsport.com/stream/772/manifest.mpd"
+  },
+  "Tsn4w": {
+    "file": "https://cors-proxy.cooks.fyi/https://livecdn-iptv001.rumsport.com/stream/773/manifest.mpd"
+  },
+  "Tsn5w": {
+    "file": "https://cors-proxy.cooks.fyi/https://livecdn-iptv001.rumsport.com/stream/774/manifest.mpd"
+  },
 };
 
 function getManifestUri(id) {
