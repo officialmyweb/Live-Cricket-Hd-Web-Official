74a184a08321bca0d36b77ffce86d7165e50376d
crushxguru <adarshmishra.pj@gmail.com>
2024-09-25 00:56:41 +0530

    Create shakavast.js

---
+++ shakavast.js
@@ -0,0 +1,47 @@
+const channelData = {
+  "laligatv2": {
+    "file": "https://live.ll.ww.aiv-cdn.net/OTTB/dub-nitro/live/clients/dash/enc/woujvkfnmn/out/v1/141b52f08a1e4e97850219729ee48dc8/cenc.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "0b1fdeaee3ffc51e9a66cf1938d57aaf",
+        "key": "28e3cb88ab7b476b81ab8aa0624c4d71"
+      }
+    }
+  },
+    "laligatv1": {
+    "file": "https://ottb.live.cf.ww.aiv-cdn.net/dub-nitro/live/clients/dash/enc/tu06vubgf9/out/v1/7668f47d28e345979c072e361f3d3ce3/cenc.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "6d08b265a6639e32da7c880d5491e3d2",
+        "key": "0c92556ca14b95f367993710e7cfbc94"
+      }
+    }
+  },
+  "laligatv": {
+    "file": "https://ottb.live.cf.ww.aiv-cdn.net/dub-nitro/live/clients/dash/enc/fpndxmzw6o/out/v1/70a50b1bda944628b8e7e66ab4069419/cenc.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "fc12a94c41e0885dbea8a8d94f0a277d",
+        "key": "fc045df077e7669666b1b230e9aa3901"
+      }
+    }
+  },
+  "dlaliga": {
+    "file": "https://live.ll.ww.aiv-cdn.net/OTTB/dub-nitro/live/clients/dash/enc/wjgklbtvhh/out/v1/659736a1e24c40e4865a80ffd75e7de7/cenc.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "43d1c3b25207ff38b22ccfe17d302367",
+        "key": "7b1f85f6e81059473b114c16a25c829a"
+      }
+    }
+  },
+  "dlaliga": {
+    "file": "https://live.ll.ww.aiv-cdn.net/OTTB/dub-nitro/live/clients/dash/enc/wjgklbtvhh/out/v1/659736a1e24c40e4865a80ffd75e7de7/cenc.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "43d1c3b25207ff38b22ccfe17d302367",
+        "key": "7b1f85f6e81059473b114c16a25c829a"
+      }
+    }
+  },
+};
