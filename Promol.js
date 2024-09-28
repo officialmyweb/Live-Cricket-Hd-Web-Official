b7648b4c550e81cd9e85f33d06a399e72dd67108
crushxguru <adarshmishra.pj@gmail.com>
2024-09-29 00:43:38 +0530

    Update Promol.js

--- Promol.js
+++ Promol.js
@@ -8,12 +8,12 @@ const channelData = {
       }
     }
   },
-    "telusa": {
-    "file": "https://fsly.stream.peacocktv.com/Content/CMAF_OL1-CTR-4s/Live/channel(kvea)/master.mpd",
+    "arena2p": {
+    "file": "https://webtvstream.bhtelecom.ba/hls6/as_premium2.mpd",
     "drm": {
       "clearkey": {
-        "keyId": "dfb59142ce523a6c900758d5ee4c7997",
-        "key": "bd2d684e590815190eda056a6d9618bc"
+        "keyId": "c18b6aa739be4c0b774605fcfb5d6b68",
+        "key": "e41c3a6f7532b2e3a828d9580124c89d"
       }
     }
    },
