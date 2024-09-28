ad0db1ff4e4d98cd0fdcd4cc13750b7b436a51c5
crushxguru <adarshmishra.pj@gmail.com>
2024-09-28 19:27:45 +0530

    Update Promol.js

--- Promol.js
+++ Promol.js
@@ -1,1 +1,38 @@
-
+const channelData = {
+  "Usanetwork": {
+    "file": "https://cfrt.stream.peacocktv.com/Content/CMAF_OL1-CTR-4s/Live/channel(usa-west)/master.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "252a671825ba31ec8433f978c32ccf6d",
+        "key": "ee560759ecc8d2274df2e63fcef56915"
+      }
+    }
+  },
+    "telusa": {
+    "file": "https://fsly.stream.peacocktv.com/Content/CMAF_OL1-CTR-4s/Live/channel(kvea)/master.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "dfb59142ce523a6c900758d5ee4c7997",
+        "key": "bd2d684e590815190eda056a6d9618bc"
+      }
+    }
+   },
+  "fox2": {
+    "file": "https://linear.stvacdn.spectrum.com/LIVE/5099/bpk-tv/00564/drm/manifest.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "791913a3304b58ba855fffbb7095a36c",
+        "key": "3d6c3ecfdbb0fbdb5f736703bea66ca1"
+      }
+    }
+  },
+  "nbc2": {
+    "file": "https://fsly.stream.peacocktv.com/Content/CMAF_OL1-CTR-4s/Live/channel(universo-east)/master.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "232d71bb013d39ffb92ee8c576fb7e8e",
+        "key": "087408a48be8d57046c3868b37ea90ba"
+      }
+    }
+  },
+};
