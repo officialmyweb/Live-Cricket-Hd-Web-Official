9ce57c170269543292d8fd094f1bbc2359ce9148
crushxguru <adarshmishra.pj@gmail.com>
2024-10-13 21:27:08 +0530

    Update Promol.js

--- Promol.js
+++ Promol.js
@@ -8,12 +8,12 @@ const channelData = {
       }
     }
   },
-    "arena2p": {
-    "file": "https://webtvstream.bhtelecom.ba/hls6/as_premium2.mpd",
+    "cric": {
+    "file": "https://worldwide-mpd.onmailkardobhai.workers.dev/proxy/https://linear007-gb-dash1-prd-ak.cdn.skycdp.com/016a/Content/DASH_003_720_120/Live/channel(skysportscricket)/manifest_720-120.mpd",
     "drm": {
       "clearkey": {
-        "keyId": "c18b6aa739be4c0b774605fcfb5d6b68",
-        "key": "e41c3a6f7532b2e3a828d9580124c89d"
+        "keyId": "00053ac1e9b1231eed3eb4e3540b3ae1",
+        "key": "0f03d8541826af3d9acbac975d751165"
       }
     }
    },
