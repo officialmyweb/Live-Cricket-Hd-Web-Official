9031439762c810ea463fcd4bc61ad5db67447146
crushxguru <adarshmishra.pj@gmail.com>
2024-10-19 19:35:41 +0530

    Update Promol.js

--- Promol.js
+++ Promol.js
@@ -35,4 +35,40 @@ const channelData = {
       }
     }
   },
+    "fl": {
+    "file": "https://fsly.stream.peacocktv.com/Content/CMAF_CTR-4s/Live/channel(lc107a1ddy)/master.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "002007110c69a23803173b50eab05f23",
+        "key": "590d6e8f4ca81319f9bb29104f571990"
+      }
+    }
+  },
+    "city": {
+    "file": "https://g006-sle-us-cmaf-prd-cf.cdn.peacocktv.com/co01/Content/CMAF_S2-CTR-4s/Live/channel(5014167-715470-48f73af7e8e)/master.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "0022e0a9aeccb5d0e7557b1c7877649e",
+        "key": "e3bc6b7edcedf7c281d86c3a4f5dd3d2"
+      }
+    }
+   },
+  "man": {
+    "file": "https://g006-sle-us-cmaf-prd-cf.cdn.peacocktv.com/co01/Content/CMAF_S2-CTR-4s/Live/channel(5014169-715472-48f73ce0310)/master.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "002295f5a27b05c8c5c4204671dd80b1",
+        "key": "b96648165f75b17238f1a1dc986927e9"
+      }
+    }
+  },
+  "town": {
+    "file": "https://g006-sle-us-cmaf-prd-cf.cdn.peacocktv.com/co01/Content/CMAF_S2-CTR-4s/Live/channel(5014166-715469-48f73a03c4d)/master.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "00225a45f6991173ea36d25fe88924be",
+        "key": "75a8f7c332aa3ea34bed1e439ed1ff68"
+      }
+    }
+  },
 };
