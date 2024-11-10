477bd473a80a07006ba1327a0867a5c273a411d5
crushxguru <adarshmishra.pj@gmail.com>
2024-11-10 19:56:25 +0530

    Update Promol.js

--- Promol.js
+++ Promol.js
@@ -8,14 +8,13 @@ const channelData = {
       }
     },
     "man": {
-    "file": "https://absuashqaabcaaayaaaaaaaaaaaaa.e693b67d00e42.cbr.cloudfront.net/co01/Content/CMAF_S2-CTR-4s-v2/Live/channel(5014718-716116-48f948718d4)/master.mpd",
-    "drm": {
-      "clearkey": {
-        "keyId": "0022e2ca432324436163049dca202a67",
-        "key": "ccfb5e0f52fbdfe25188e67afca9121c"
+      "file": "https://absuashqaabcaaayaaaaaaaaaaaaa.e693b67d00e42.cbr.cloudfront.net/co01/Content/CMAF_S2-CTR-4s-v2/Live/channel(5014718-716116-48f948718d4)/master.mpd",
+      "drm": {
+        "clearkey": {
+          "keyId": "0022e2ca432324436163049dca202a67",
+          "key": "ccfb5e0f52fbdfe25188e67afca9121c"
+        }
       }
     }
-   },
-  };
-
-    
+  }
+};
