18e25895a2f7a034da8c767f71e0780ac9c1bd83
crushxguru <adarshmishra.pj@gmail.com>
2024-11-10 20:03:07 +0530

    Update Promol.js

--- Promol.js
+++ Promol.js
@@ -1,19 +1,20 @@
 const channelData = {
-  "dazn4": {
-    "file": "https://live.ll.ww.aiv-cdn.net/OTTB/dub-nitro/live/clients/dash/enc/up7qpwch9b/out/v1/a6d5d1a1287b4893b859c2d6ccf2c65d/cenc.mpd",
+  "Usanetwork": {
+    "file": "https://cfrt.stream.peacocktv.com/Content/CMAF_OL1-CTR-4s/Live/channel(usa-west)/master.mpd",
     "drm": {
       "clearkey": {
-        "keyId": "d27104d427e4f87e75b19395a9f8796b",
-        "key": "723593c70e2d4c4862754398e80168f8"
+        "keyId": "252a671825ba31ec8433f978c32ccf6d",
+        "key": "ee560759ecc8d2274df2e63fcef56915"
       }
-    },
+    }
+  },
     "man": {
-      "file": "https://absuashqaabcaaayaaaaaaaaaaaaa.e693b67d00e42.cbr.cloudfront.net/co01/Content/CMAF_S2-CTR-4s-v2/Live/channel(5014718-716116-48f948718d4)/master.mpd",
-      "drm": {
-        "clearkey": {
-          "keyId": "0022e2ca432324436163049dca202a67",
-          "key": "ccfb5e0f52fbdfe25188e67afca9121c"
-        }
+    "file": "https://absuashqaabcaaayaaaaaaaaaaaaa.e693b67d00e42.cbr.cloudfront.net/co01/Content/CMAF_S2-CTR-4s-v2/Live/channel(5014718-716116-48f948718d4)/master.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "0022e2ca432324436163049dca202a67",
+        "key": "ccfb5e0f52fbdfe25188e67afca9121c"
       }
-   }  
-  };
+    }
+  }
+};
