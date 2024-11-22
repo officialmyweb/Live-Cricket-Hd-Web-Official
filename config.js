8920d29983deb864bc39e96c4698d3d847c529d3
crushxguru <adarshmishra.pj@gmail.com>
2024-11-22 10:11:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     manifestUri: "https://bpcdncs1.dialog.lk/bpk-token/2aa@hstrvk1aogwk3jqggbbj3hdlv4r2mbidqhfxvzca/GlobalManifest.mpd",
     drmKeys: {
-        "c699eaeebd6ac0110446bc55a52231e8": "7b7ee72122f256228bdf620c9393a049;"
+        "34fe716be62359a688c536ee89a269fb": "3e3b193051181fda9806717c192a81a3;"
     }
 };
