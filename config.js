ee45dc0dea885d517b7b93d8fc355b8aec71688a
crushxguru <adarshmishra.pj@gmail.com>
2024-11-22 06:47:09 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+    manifestUri: "https://bpcdncs1.dialog.lk/bpk-token/2aa@dvhat4ht4am05rjqqclbtpag35i3bvjguargtnca/GlobalManifest.mpd",
+    drmKeys: {
+        "c699eaeebd6ac0110446bc55a52231e8": "7b7ee72122f256228bdf620c9393a049;"
+    }
+};
