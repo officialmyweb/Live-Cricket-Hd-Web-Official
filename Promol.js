a9803cba834761559eb7bf2916f4452c8a900041
crushxguru <adarshmishra.pj@gmail.com>
2024-11-23 20:45:18 +0530

    Update Promol.js

--- Promol.js
+++ Promol.js
@@ -1,19 +1,37 @@
 const channelData = {
-  "not": {
-    "file": "https://linears-playback.astro.com.my/vsg/41xY9nwDDzKU-lgjBdVKnVoWrIs3ydNDCXUICsh2mL898=07iN/dash-wv/jitp-linearwm/2701/default.mpd",
+  "fulham": {
+    "file": "https://g001-sle-us-cmaf-prd-fy.cdn.peacocktv.com/mm/master.mpd",
     "drm": {
       "clearkey": {
-        "keyId": "5be66a6f88efee3d6e30f72a9c58e310",
-        "key": "168f3fadfc730ba3dfc932f7c430e27d"
+        "keyId": "0022258ace24bbe101d6b3804700f39e",
+        "key": "487ec4a120b7f4c11eafffb2905c2f0a"
       }
     }
   },
-    "man": {
-    "file": "https://linearjitp-playback.astro.com.my/dash-wv/linear/2506/default.mpd",
+    "everton": {
+    "file": "https://g003-sle-us-cmaf-prd-fy.cdn.peacocktv.com/co01/Content/CMAF_S2-CTR-4s-v2/Live/channel(5014728-716126-48f951faf5e)/master.mpd",
     "drm": {
       "clearkey": {
-        "keyId": "79f4028730acca9ab8b00f26158ddb10",
-        "key": "91febe843c08c7cc523efd827292e40e"
+        "keyId": "00227cbb102532dc110c778a04803450",
+        "key": "8763279501d90605a5b0616e14fac076"
+      }
+    }
+  },
+  "asernal": {
+    "file": "https://g003-sle-us-cmaf-prd-fy.cdn.peacocktv.com/co01/Content/CMAF_S2-CTR-4s-v2/Live/channel(5014726-716124-48f95012adc)/master.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "0022aa68db11bdd7252096ee046f940b",
+        "key": "74cd50d9a442cdbfe8a4404c1de0f182"
+      }
+    }
+  },
+  "aston": {
+    "file": "https://g003-sle-us-cmaf-prd-fy.cdn.peacocktv.com/co01/Content/CMAF_S2-CTR-4s-v2/Live/channel(5014727-716125-48f95106d1d)/master.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "0022720aadd6d5fc419cd92313817996",
+        "key": "28c67c9b54aab493c50bf81e1c83799e"
       }
     }
   }
