4b6a48c36b730cb1d46dee9261f4a2372991eed9
crushxguru <adarshmishra.pj@gmail.com>
2024-11-30 20:15:23 +0530

    Update Promol.js

--- Promol.js
+++ Promol.js
@@ -8,5 +8,32 @@ const channelData = {
       }
     }
   },
+  "afc": {
+    "file": "https://g006-sle-us-cmaf-prd-ak.cdn.peacocktv.com/co01/Content/CMAF_S2-CTR-4s/Live/channel(5015352-716807-48fba512e07)/master.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "0022c5301a71eb2c43a6984dd0f347aa",
+        "key": "39da5776c306d57224486b42aa6d417e"
+      }
+    }
+  },
+  "crystal": {
+    "file": "https://g006-sle-us-cmaf-prd-ak.cdn.peacocktv.com/co01/Content/CMAF_S2-CTR-4s/Live/channel(5014928-716341-48fa10b7235)/master.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "0022a7f21aaebf636469d51ef39b0f6a",
+        "key": "bac20704f3b30cae56849997823b1d67"
+      }
+    }
+  },
+  "city": {
+    "file": "https://g006-sle-us-cmaf-prd-cf.cdn.peacocktv.com/co01/Content/CMAF_S2-CTR-4s/Live/channel(5014927-716340-48fa0fc2ff4)/master.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "00224628fcbd12d9c57f32e5ed60048c",
+        "key": "ca9a281bd584e41472238b7f4ba0af7d"
+      }
+    }
+  },
 };
   
