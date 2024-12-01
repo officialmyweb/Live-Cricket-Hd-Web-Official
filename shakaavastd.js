3f6d3699191d44f96dd7ceceb0aaaacbbbb73b82
crushxguru <adarshmishra.pj@gmail.com>
2024-12-01 19:50:34 +0530

    Update shakaavastd.js

--- shakaavastd.js
+++ shakaavastd.js
@@ -1,19 +1,28 @@
 const channelData = {
-  "Usanetwork": {
-    "file": "https://cfrt.stream.peacocktv.com/Content/CMAF_OL1-CTR-4s/Live/channel(usa-west)/master.mpd",
+  "abein3": {
+    "file": "https://linearjitp-playback.astro.com.my/dash-wv/linear/2705/default.mpd",
     "drm": {
       "clearkey": {
-        "keyId": "252a671825ba31ec8433f978c32ccf6d",
-        "key": "ee560759ecc8d2274df2e63fcef56915"
+        "keyId": "20c884ef8ed26b2762f8b1a78f2d1910",
+        "key": "042a21bf236ca729b1e343ed6e0a6337"
       }
     }
   },
-  "Usanetwork": {
-    "file": "https://cfrt.stream.peacocktv.com/Content/CMAF_OL1-CTR-4s/Live/channel(usa-west)/master.mpd",
+  "abein2": {
+    "file": "https://linearjitp-playback.astro.com.my/dash-wv/linear/5066/default.mpd",
     "drm": {
       "clearkey": {
-        "keyId": "252a671825ba31ec8433f978c32ccf6d",
-        "key": "ee560759ecc8d2274df2e63fcef56915"
+        "keyId": "39c175581e237eff9559607eb9b23c10",
+        "key": "5102b12aac7756c65dcb46a101d960d3"
+      }
+    }
+  },
+    "abein": {
+    "file": "https://linearjitp-playback.astro.com.my/dash-wv/linear/408/default.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "1a655189ab5c49eb235308c2b1a9e710",
+        "key": "3c4508af348844107f5e026a4fd6b16e"
       }
     }
   },
