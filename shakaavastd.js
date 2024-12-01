46a08141964e25b7bff6ddd5fd0eca385c4e9b8b
crushxguru <adarshmishra.pj@gmail.com>
2024-12-01 19:27:56 +0530

    Create shakaavastd.js

---
+++ shakaavastd.js
@@ -0,0 +1,21 @@
+const channelData = {
+  "Usanetwork": {
+    "file": "https://cfrt.stream.peacocktv.com/Content/CMAF_OL1-CTR-4s/Live/channel(usa-west)/master.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "252a671825ba31ec8433f978c32ccf6d",
+        "key": "ee560759ecc8d2274df2e63fcef56915"
+      }
+    }
+  },
+  "Usanetwork": {
+    "file": "https://cfrt.stream.peacocktv.com/Content/CMAF_OL1-CTR-4s/Live/channel(usa-west)/master.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "252a671825ba31ec8433f978c32ccf6d",
+        "key": "ee560759ecc8d2274df2e63fcef56915"
+      }
+    }
+  },
+};
+  
