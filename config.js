e141a080954f5b033cac5cbea368177248be17ce
crushxguru <adarshmishra.pj@gmail.com>
2024-12-06 01:03:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    manifestUri: "https://proxy.animebash.workers.dev/?u=http://livecrichdofficial.42web.io/mpd.php",
+    manifestUri: "https://proxy.animebash.workers.dev/?u=http://livecrichdofficial.42web.io/mod.php",
     drmKeys: {
         "be00709d621658fb80a5d70b49b2fe85": "c4edb42199a35fc7ab08ecca1cb0828d;"
     }
