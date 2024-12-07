9d759956aeca7f2720cc4edc60494d3ff2b78bf4
crushxguru <adarshmishra.pj@gmail.com>
2024-12-07 20:35:42 +0530

    Update Promol.js

--- Promol.js
+++ Promol.js
@@ -1,11 +1,20 @@
 const channelData = {
-  "fulham": {
-    "file": "https://g006-sle-us-cmaf-prd-ak.cdn.peacocktv.com/co01/Content/CMAF_S2-CTR-4s/Live/channel(5015364-716819-48fbb084913)/master.mpd",
+  "mancity": {
+    "file": "https://g006-sle-us-cmaf-prd-qw.cdn.peacocktv.com/co01/Content/CMAF_S2-CTR-4s/Live/channel(5015371-716826-48fbb7318da)/master.mpd",
     "drm": {
       "clearkey": {
-        "keyId": "0022745c106f50f7e45c39a44ec56ae8",
-        "key": "ab421b4a121c096ed9e5ce729bcfe0b9"
+        "keyId": "00222225698efca4050216676fb44cbf",
+        "key": "5b46cfccd72d229931b0544e18b540f2"
       }
     }
   },
+    "aston": {
+    "file": "https://g006-sle-us-cmaf-prd-fy.cdn.peacocktv.com/co01/Content/CMAF_S2-CTR-4s/Live/channel(5015370-716825-48fbb63d699)/master.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "0022129ee6cf99a37c791d884acacf91",
+        "key": "612af87b97b6f3f0cbae521e81225340"
+      }
+    }
+   }
 };
