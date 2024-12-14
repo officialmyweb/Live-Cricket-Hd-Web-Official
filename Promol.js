f3dcf8c69351becd4cb2ac08bb5c8aa7bc83f775
crushxguru <adarshmishra.pj@gmail.com>
2024-12-14 20:14:47 +0530

    Update Promol.js

--- Promol.js
+++ Promol.js
@@ -1,19 +1,28 @@
 const channelData = {
-  "ips": {
-    "file": "https://cfrt.stream.peacocktv.com/Content/CMAF_OL1-CTR-4s/Live/channel(usa-west)/master.mpd",
+  "asernal": {
+    "file": "https://g006-sle-us-cmaf-prd-cf.cdn.peacocktv.com/co01/Content/CMAF_S2-CTR-4s-v2/Live/channel(5015414-716876-48fbe0339cc)/master.mpd",
     "drm": {
       "clearkey": {
-        "keyId": "252a671825ba31ec8433f978c32ccf6d",
-        "key": "ee560759ecc8d2274df2e63fcef56915"
+        "keyId": "00229402fd5fc18ae12fd751f524f44e",
+        "key": "d0a94a52485ae57547d3e2b325a9696e"
       }
     }
   },
-  "asernal": {
-    "file": "https://cfrt.stream.peacocktv.com/Content/CMAF_OL1-CTR-4s/Live/channel(usa-west)/master.mpd",
+  "liverpool": {
+    "file": "https://g006-sle-us-cmaf-prd-cf.cdn.peacocktv.com/co01/Content/CMAF_S2-CTR-4s-v2/Live/channel(5015415-716877-48fbe127c0d)/master.mpd",
     "drm": {
       "clearkey": {
-        "keyId": "252a671825ba31ec8433f978c32ccf6d",
-        "key": "ee560759ecc8d2274df2e63fcef56915"
+        "keyId": "00224cd383e9182089710f20e4f305e3",
+        "key": "189eefbb460682d0934814f504c4c078"
+      }
+    }
+  },
+    "ips": {
+    "file": "https://g006-sle-us-cmaf-prd-cf.cdn.peacocktv.com/co01/Content/CMAF_S2-CTR-4s-v2/Live/channel(5015415-716877-48fbe127c0d)/master.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "00224cd383e9182089710f20e4f305e3",
+        "key": "189eefbb460682d0934814f504c4c078"
       }
     }
   },
