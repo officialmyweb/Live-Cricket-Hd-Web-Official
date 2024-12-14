fb0d935283ac3b144068a48f2496f209cd428fc9
crushxguru <adarshmishra.pj@gmail.com>
2024-12-14 20:20:37 +0530

    Update Promol.js

--- Promol.js
+++ Promol.js
@@ -17,12 +17,21 @@ const channelData = {
       }
     }
   },
-    "ips": {
-    "file": "https://g006-sle-us-cmaf-prd-cf.cdn.peacocktv.com/co01/Content/CMAF_S2-CTR-4s-v2/Live/channel(5015415-716877-48fbe127c0d)/master.mpd",
+    "usanetwork": {
+    "file": "https://fsly.stream.peacocktv.com/Content/CMAF_OL1-CTR-4s/Live/channel(usa-east)/master.mpd",
     "drm": {
       "clearkey": {
-        "keyId": "00224cd383e9182089710f20e4f305e3",
-        "key": "189eefbb460682d0934814f504c4c078"
+        "keyId": "91bf2d81b9e13701ac667915641121b8",
+        "key": "d0cc2082132b430ad5a8bc4f31c9b772"
+      }
+    }
+  },
+  "ips": {
+    "file": "https://fsly.stream.peacocktv.com/Content/CMAF_OL1-CTR-4s/Live/channel(usa-east)/master.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "91bf2d81b9e13701ac667915641121b8",
+        "key": "d0cc2082132b430ad5a8bc4f31c9b772"
       }
     }
   },
