770ac84e5e95f4872814cb378e299b0410be458a
crushxguru <adarshmishra.pj@gmail.com>
2024-12-15 11:22:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    manifestUri: "https://g006-sle-us-cmaf-prd-cf.cdn.peacocktv.com/co01/Content/CMAF_S2-CTR-4s-v2/Live/channel(5015486-716956-48fc24ddc1c)/master.mpd",
+    manifestUri: "https://cors-anywhere.ammoapps.com/https://dfwlive-v1-c1p3-sponsored.akamaized.net/Content/HLS.cps/Live/channel(WILLXT-3292.dfw.1080)/index.m3u8",
     drmKeys: {
         "002261d4b0275c483fcea5311150282b": "7c48ad9f147e13b594bb6125c60112a6;"
     }
