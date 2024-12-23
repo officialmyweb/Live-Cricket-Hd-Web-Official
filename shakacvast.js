c9300cf0eb8b683d22977fe0b914380afe52d126
crushxguru <adarshmishra.pj@gmail.com>
2024-12-23 13:55:55 +0530

    Update shakacvast.js

--- shakacvast.js
+++ shakacvast.js
@@ -1,11 +1,1 @@
-const channelData = {
-  "premier1": {
-    "file": "https://livecric.alvbastripathi1.workers.dev/https://linear001-ie-dash1-prd-ak.cdn.skycdp.com/016a/Content/DASH_003_720_120/Live/channel(premiersports1)/manifest_720.mpd",
-    "drm": {
-      "clearkey": {
-        "keyId": "0005b3dba78ae0fe7a9288022d3b3736",
-        "key": "d2827df80dd7e9243ba6b5bbda5dfa82"
-      }
-    }
-  }
-};
+.
