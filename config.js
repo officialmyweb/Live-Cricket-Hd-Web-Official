e17fe8772a1d8acfb9f567eb8d9d3979c0683120
crushxguru <adarshmishra.pj@gmail.com>
2025-08-01 13:46:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,1 @@
-const CONFIG = {
-    manifestUri: "https://cors-anywhere.ammoapps.com/https://dfwlive-v1-c1p3-sponsored.live.cf.dtvcdn.com/Content/HLS.cps/Live/channel(WILLXT-3292.dfw.1080)/index.m3u8",
-    drmKeys: {
-        "08fbdb19627b6eacb20efbd39486050d": "7818829c2a81fa316a48fb9150ab21d3;"
-    }
-};
+.
