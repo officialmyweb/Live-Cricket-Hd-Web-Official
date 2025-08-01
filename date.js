ca21dbc2182ba4a86c346b3224030d111cc674c0
crushxguru <adarshmishra.pj@gmail.com>
2025-08-01 13:45:19 +0530

    Update date.js

--- date.js
+++ date.js
@@ -1,16 +1,1 @@
-// date.js - External script to dynamically add today's date and year
-window.onload = function() {
-    const dateElement = document.getElementById('date-display');
-    const today = new Date();
-    
-    // Format today's date as "Month Day, Year"
-    const formattedDate = today.toLocaleDateString('en-US', {
-        weekday: 'long', // e.g. "Monday"
-        month: 'long', // e.g. "December"
-        day: 'numeric',
-        year: 'numeric',
-    });
-
-    // Set the formatted date to the white box
-    dateElement.innerHTML = `${formattedDate}`;
-}
+.
